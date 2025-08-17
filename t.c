040188f9c40b9ee2a16009840553a68c0f2a0c44
yaraPB <yarakouttane234@gmail.com>
2025-08-17 20:08:19 +0100

    Sending messages in UDP

---
+++ t.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    int i = 9;
+    char* m = "Hello"+ i;
+    printf("%s\n", m);
+}
--- udp.c
+++ udp.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 int main(){
     printf("UDP Message Sender\n");
-
-    const char* message = "Hello";
     
-    // number of characters 
-    int size_char = sizeof(message)/sizeof(message[0]);
-
+    
     // Send message to phone
-    sendto(int socket, message, sizeof(char*)*strlen(message),0,
-    const struct sockaddr *address, size_t address_len);
+    int socket_id = socket(AF_INET, SOCK_DGRAM, 0);
+    printf("Socket = %d\n", socket_id);
+    
+    struct sockaddr_in sin;
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(12345);
+    sin.sin_addr.s_addr = inet_addr("192.168.100.21");
+    
+    
+    for (int i=0; i<100; i++){
+    const char* message = "Message from the computer";
+    sendto(socket_id, message, strlen(message),0,
+    (struct sockaddr *)&sin, sizeof(sin));
+    }
+
 }
